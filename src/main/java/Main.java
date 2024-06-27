49c62b87297e4f926ecf1aa8c43a3227a4483cf7
CITATS928 <93238522+CITATS928@users.noreply.github.com>
2024-06-27 14:22:50 -0400

    gaussian is faster

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  * Modified by kristofer in Jan 2024
@@ -5,23 +7,47 @@
 public class Main {
 
     public static void main(String[] args){
-        
+        long st,et,et2,at,at2;
         int number = getInputNumber();
-        
+
+        st=System.currentTimeMillis();
         int sum = sumOfNumbers(number);
-        
-        System.out.println(sum);
+        et=System.currentTimeMillis();
+        int sum2=gaussianSumOfNumbers(number);
+        et2=System.currentTimeMillis();
+        at=et-st;
+        at2=et2-et;
+        System.out.println("sum is "+sum+"\ngaussian is "+sum2);
+        System.out.println("st:"+st+"\net:"+et+"\net2:"+et2);
+
+        if(at>at2){
+            System.out.println("2 fast");
+        } else if (at2>at) {
+            System.out.println("1 fast");
+        }else{
+            System.out.println("same");
+        }
 
     }
-    
+
     // do this one and the lab is complete. really.
     // return the sum of 0 to n... if n == 3, the result should be 6
-    int sumOfNumbers(int n) {
-        return 0;
+    static int sumOfNumbers(int n) {
+        int sum=0;
+/*        long st,et,at;
+        st=System.currentTimeMillis();*/
+        for(int i=n;i>=0;i--){
+            sum+=n;
+            n--;
+        }
+/*        et = System.currentTimeMillis();
+        at = et - st;
+        System.out.println("actual time is "+at);*/
+        return sum;
     }
     
     
-    int getInputNumber() { // gee, may thi sis useful in another lab?
+    static int getInputNumber() { // gee, may thi sis useful in another lab?
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter any number: ");
 
@@ -36,8 +62,18 @@ public class Main {
     
     
     // for Extra Credit
-    int gaussianSumOfNumbers(int n) {
-        return 0;
+    static int gaussianSumOfNumbers(int n) {
+        int sum;
+/*        long st,et,at;
+        st=System.currentTimeMillis();*/
+        sum=(n*(n+1))/2;
+
+/*        et = System.currentTimeMillis();
+        at = et - st;
+        System.out.println("actual time is "+at);*/
+
+
+        return sum;
     }
    
     // for Extra extra credit - compare the methods and show which one is faster
